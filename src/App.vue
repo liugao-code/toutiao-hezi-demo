d4c9f9c9bf27805e730336d87f98f3d89a1a545f
start-learm <776281413@qq.com>
2021-06-19 20:55:56 +0800

    项目初始化-调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+    <h1>黑马头条</h1>
+    <!-- 路由出口 -->
+		<router-view />
+	</div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name:'App',
+	data() {
+		return {};
+	},
 
-#nav {
-  padding: 30px;
+	created() {},
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+	methods: {},
+};
+</script>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style  scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
+/* 
+项目的启动入口
+*/
 import Vue from 'vue'
+// 导入根组件
 import App from './App.vue'
+// 导入路由
 import router from './router'
-
+// 导入全局样式文件
+import './styles/index.less';
 Vue.config.productionTip = false
-
+// 创建 vue 根实例
+// 把 router 配置到根实例中
+// 通过 render 方法把 App 根组件渲染到 #app 入口节点
 new Vue({
   router,
   render: h => h(App)
+  // el: 'app' // 等价于 $mound('app')
 }).$mount('#app')
